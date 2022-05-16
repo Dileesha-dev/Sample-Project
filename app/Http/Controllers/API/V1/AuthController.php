186db3cc71900901d1da13e1b7d6981de321a578
Dileesha_Dev <dileeshanayanajith123@gmail.com>
2022-05-16 11:48:43 +0530

    added docs for api

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -8,8 +8,36 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * @group Authentication
+ *
+ * API endpoints for managing authentication
+ */
+
 class AuthController extends Controller
 {
+    /**
+     * Register the user.
+     *
+     * @bodyParam   name    string  required    The full name of the  user.      Example: Mike Tyson
+     * @bodyParam   email    string  required    The email of the  user.      Example: testuser@example.com
+     * @bodyParam   password    string  required    The password of the  user.   Example: secret
+     * @bodyParam   password_confirmation    string  required    The password confirmation of the  user.   Example: secret
+     *
+     * @response {
+     * 
+     *  "user": {
+     *      "id": 2,
+     *      "name": "Jack Sparrow",
+     *      "email": "jack@sparrow.ship",
+     *      "email_verified_at": null,
+     *      "is_admin": 0,
+     *       "created_at": "2022-05-16T05:12:01.000000Z",
+     *       "updated_at": "2022-05-16T05:12:01.000000Z"
+     * },
+     *  "token": "4|UuwRyt2CMfNkxdr44Y1wuORDzz70HhuFvblR67uC",
+     * }
+     */
     public function register(Request $request){
         $data = $request->validate([
             'name' => 'required|string',
@@ -32,6 +60,28 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
+
+    /**
+     * Log in the user.
+     *
+     * @bodyParam   email    string  required    The email of the  user.      Example: testuser@example.com
+     * @bodyParam   password    string  required    The password of the  user.   Example: secret
+     *
+     * @response {
+     * 
+     *  "user": {
+     *      "id": 2,
+     *      "name": "Jack Sparrow",
+     *      "email": "jack@sparrow.ship",
+     *      "email_verified_at": null,
+     *      "is_admin": 0,
+     *       "created_at": "2022-05-16T05:12:01.000000Z",
+     *       "updated_at": "2022-05-16T05:12:01.000000Z"
+     * },
+     *  "token": "4|UuwRyt2CMfNkxdr44Y1wuORDzz70HhuFvblR67uC",
+     * }
+     */
     
     public function login(Request $request){
         $data = $request->validate([
@@ -57,6 +107,14 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    /**
+     * Logout the user.
+     *
+     * @response {
+     * 
+     *  "message": "Logged Out"
+     * }
+     */
     public function logout(Request $request){
         auth()->user()->tokens()->delete();
 
@@ -65,6 +123,23 @@ class AuthController extends Controller
         ];
     }
     
+    /**
+     * Profile of the user.
+     *
+     * @response {
+     * 
+     *  "payload": {
+     *      "id": 2,
+     *      "name": "Jack Sparrow",
+     *      "email": "jack@sparrow.ship",
+     *      "email_verified_at": null,
+     *      "is_admin": 0,
+     *       "created_at": "2022-05-16T05:12:01.000000Z",
+     *       "updated_at": "2022-05-16T05:12:01.000000Z"
+     *      },
+     *      "message": "Profile info"
+     * }
+     */
     public function profile(){
         return response([
             'payload' => auth()->user(),
